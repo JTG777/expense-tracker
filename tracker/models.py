db58493c3f3871a07675a8367c121a69aa4619e9
Joseph T George <120744239+JTG777@users.noreply.github.com>
2025-06-29 12:14:31 +0530

    add html,css and define models

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class CurrentBalance(models.Model):
+    current_balance=models.FloatField(default=0)
+    
+
+class TrackingHistory(models.Model):
+    current_balance=models.ForeignKey(CurrentBalance,on_delete=models.CASCADE)
+    amount=models.FloatField()
+    expense_type=models.CharField(max_length=50,choices=(('credit','credit'),('debit','debit')))
+    description=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
